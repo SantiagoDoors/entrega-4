e5d2673af4006dd6de656860dd55499e73749341
Santi <antratocx@gmail.com>
2023-03-12 22:24:06 +0100

    completos

--- C4_Ejercicio_2.java
+++ C4_Ejercicio_2.java
@@ -1,16 +1,26 @@
 import java.util.Arrays;
 import java.util.Collections;
+import javax.swing.JOptionPane;
 
-public class C4_Ejercicio_1 {
+public class C4_Ejercicio_2 {
     public static void main(String[] args) {
 
     // ingresar los numeros a ordenar
 
-        int num1 = 23, num2 = 98, num3 = 2; 
+        String p = JOptionPane.showInputDialog("ingrese el primer número");
+        String s = JOptionPane.showInputDialog("ingrese el segundo número");
+        String t = JOptionPane.showInputDialog("ingrese el tercer número");
+
+    //convertir string a int
+
+        int num1 = Integer.parseInt(p);
+        int num2 = Integer.parseInt(s);
+        int num3 = Integer.parseInt(t);
 
     // ingresar la forma de orden
 
-        String asc = "d";
+        String asc = JOptionPane.showInputDialog("ingrese el tipo de orden preferido siendo 'A' Ascendente, o 'D' Descendente").toLowerCase();
+
 
     //ordenar de forma Ascendente
 
