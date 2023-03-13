b41addb859fecc224a764758fa48457b7d7d56d5
Santi <antratocx@gmail.com>
2023-03-13 17:54:46 +0100

    Par o impar

---
+++ C4_Ejercicio_4.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+//         Ejercicio Nº 1 Averiguar si un número es divisible por otro, 
+//         utilizando el módulo o resto de la división (%).
+
+public class C4_Ejercicio_4{
+    public static void main(String[] args) {
+        JOptionPane.showInputDialog("_[Par o Impar]_");
+        String a = JOptionPane.showInputDialog("ingrese un numero y le dire!! muajaaaaaa ja!");
+        int ini = Integer.parseInt(a);
+        if (ini %2 == 0) {
+            JOptionPane.showInputDialog("su numero es _[Par]_ ");
+        }
+        JOptionPane.showInputDialog("su numero es _[Impar]_ ");
+    }
+}
