0033a83e8f47ff81bf476c9aac159b8d3a8d3472
Santi <antratocx@gmail.com>
2023-03-13 17:37:18 +0100

    C4_Ejercicio_3

--- C4_Ejercicio_3.java
+++ C4_Ejercicio_3.java
@@ -1,6 +1,17 @@
+import javax.swing.JOptionPane;
+
 public class C4_Ejercicio_3 {
     public static void main(String[] args) {
+                //Pedir Variable inicio, y de Finalizacion.
+                String a = JOptionPane.showInputDialog("ingrese un numero de inicio");
+                String b = JOptionPane.showInputDialog("ingrese un numero de fin");
+                int ini = Integer.parseInt(a);
+                int fin = Integer.parseInt(b);
 
-
+                System.out.println("Numeros desde ini a fin");
+                while (ini < fin){
+                    ini += 1;
+                    System.out.println(ini);
+        }
     }
-}
+} 
